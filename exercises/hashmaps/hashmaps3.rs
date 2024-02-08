eb8e8f4be58b19a6637017ebabc16084765e4c7a
shikuan.sk <shikuan.sk@antgroup.com>
2024-02-08 11:20:32 +0800

    fix

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,7 +37,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-    }
+        if(scores.get(&team_1_name).is_none()){
+            let team = Team{
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            };
+            scores.insert(team_1_name, team);
+        }else{
+            let team = scores.get_mut(&team_1_name).unwrap();
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }
+        if(scores.get(&team_2_name).is_none()){
+            let team = Team{
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score
+            };
+            scores.insert(team_2_name, team);
+        }else{
+            let team = scores.get_mut(&team_2_name).unwrap();
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        }
+
+    }   
     scores
 }
 
--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -30,6 +28,13 @@ fn create_order_template() -> Order {
     }
 }
 
+impl Order {
+    fn update(&mut self, count: u32,name:String) {
+        self.count = count;
+        self.name = name;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,7 +43,8 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let mut your_order = create_order_template();
+        your_order.update(1,"Hacker in Rust".to_string());
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
